11c1d30a1465d4352ddb03f4a81d619e33f7b6b8
knockotd <anjeundeun@gmail.com>
2019-05-22 21:23:02 +0900

    Initial Commit

---
+++ SwipeNOOne/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SwipeNOOne
+//
+//  Created by office on 22/05/2019.
+//  Copyright © 2019 수리. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwipeNOOne/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SwipeNOOne
+//
+//  Created by office on 22/05/2019.
+//  Copyright © 2019 수리. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
